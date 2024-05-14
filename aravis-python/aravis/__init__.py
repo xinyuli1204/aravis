45a1f13e033cda7e2bd993438d585174dab7ca5e
fixstars_xinyu <143661893+xinyuli1204@users.noreply.github.com>
2024-05-13 17:30:45 -0700

    fix/windows and linux

--- aravis-python/aravis/__init__.py
+++ aravis-python/aravis/__init__.py
@@ -3,9 +3,10 @@ import ctypes
 import platform
 pre_built_path = os.path.dirname(__file__)
 if platform.system() == 'Windows':
+    os.add_dll_directory(os.path.join(pre_built_path, "bin"))
     aravis_module = os.path.join(pre_built_path, "bin/aravis-0.8-0.dll")
 elif platform.system() == 'Darwin':
-    aravis_module = os.path.join(pre_built_path, 'libaravis-0.8.so')
+    aravis_module = os.path.join(pre_built_path, 'lib/libaravis-0.8.dylib')
 elif platform.system() == 'Linux':
     aravis_module = os.path.join(pre_built_path, "lib/libaravis-0.8.so")
 
